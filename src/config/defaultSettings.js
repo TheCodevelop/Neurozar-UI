4f3a71adcfc09b32012381d21e2f30f4037fdb26
TheCodevelop <sebastiansalazarp27@gmail.com>
2026-06-01 17:35:50 -0400

    saving changes

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -12,7 +12,7 @@
  *
  */
 
-export const PYTHON_API_BASE_URL = process.env.VUE_APP_PYTHON_API_BASE_URL || 'http://localhost:5000'
+export const PYTHON_API_BASE_URL = process.env.VUE_APP_PYTHON_API_BASE_URL || 'http://127.0.0.1:5000'
 
 export default {
   /** Web UI release label (footer, docs cross-reference). */
